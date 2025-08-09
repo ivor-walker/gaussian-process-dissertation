0c8391b719dd2d6b901c9d94e5b4f92e5581c2e5
Ivor Walker <ivorivor1@hotmail.com>
2025-08-09 03:40:52 +0100

    Finished final draft of covariance functions chapter

--- code/exploring-covariance-functions.R
+++ code/exploring-covariance-functions.R
@@ -1,10 +1,11 @@
 library(GauPro)
 library(tidyverse)
+library(latex2exp)
 
 set.seed(1234)
 
 # Create the data
-n <- 20
+n <- 15
 x <- seq(0, 1, l=n)
 dimensions <- 1
 
@@ -27,6 +28,7 @@ exp_fx <- function(x) {
       )
   )
 }
+exp_fx <- exp_fx(x)
   
 # Matern 3/2: smooth wave + shallow kink
 m32_fx <- se_fx + 0.2 * abs(x - 0.5)
@@ -38,16 +40,21 @@ m52_fx <- se_fx * exp(-x)
 # Assemble all functions into vector
 stationary_fxs <- cbind(
   se_fx,
-  rq_fx,
-  ge_fx,
-  exp_fx(x),
-  m32_fx,
-  m52_fx
+  exp_fx
+)
+function_names <- c("Smooth", "Rough")
+
+# Add noise to each column of results
+sigma_n <- 0.05
+noise_matrix <- matrix(
+  rnorm(
+    n = nrow(stationary_fxs) * ncol(stationary_fxs),
+    mean = 0,
+    sd = sigma_n
+  ), ncol = ncol(stationary_fxs)
 )
 
-# Add noise to all results
-sigma_n <- 0.05
-stationary_ys <- stationary_fxs + rnorm(n, sd = sigma_n)
+stationary_ys <- stationary_fxs + noise_matrix
 
 # Define all kernels to use
 stationary_kernels <- c(
@@ -66,69 +73,84 @@ nonstationary_kernels <- c(
 
 factor_kernels <- c()
 
-dataset_index <- 1
-# Fit a Gaussian process model using each supplied kernel and each supplied dataset
+
+# Fit a Gaussian process model using each stationary kernel and each supplied dataset
+
+# For each kernel
 stationary_models <- lapply(
   stationary_kernels,
-  function(kernel) {
-
-    # Fit the model to the stationary models data
-    model <- gpkm(x, ys[,dataset_index],
-      kernel = kernel
+  function(stationary_kernel) {
+    
+    # For each dataset
+    kernel_models <- lapply(
+      seq_along(function_names),
+      function(dataset_index){
+        # Fit the model to the stationary models data
+        model <- gpkm(
+          X = x,
+          Z = stationary_ys[, dataset_index],
+          kernel = stationary_kernel$clone(deep = TRUE)
+        )
+        
+        # Add the function evaluations to the model object and return it
+        attr(model, "function_fx") <- stationary_fxs[,dataset_index]
+        attr(model, "function_name") <- function_names[dataset_index]
+        
+        return (model)
+      }
     )
     
-    # Add the function evaluations to the model object and return it
-    attr(model, "fx") <- fxs[,dataset_index]
-    
-    dataset_index <<- dataset_index + 1
-    
-    return (model)
+    return(kernel_models)
   }
 )
 
-nonstationary_models <- lapply(
-  nonstationary_kernels,
-  function(kernel) {
-    
-    model <- gpkm(x, y,
-      kernel = kernel
-    )
-    
-    attr(model, "fx") <- fx
-    return (model)
-  }
-)
-
-factor_models <- lapply(
-  factor_kernels,
-  function(kernel) {
-    
-    model <- gpkm(x, y,
-      kernel = kernel
-    )
-    
-    attr(model, "fx") <- fx
-    return (model)
-  }
-)
+# Flatten
+stationary_models <- unlist(stationary_models, recursive = FALSE)
 
 # Plot the results and print model summaries
-models <- c(stationary_models, nonstationary_models, factor_models)
+models <- stationary_models
 
 # Define xlim and ylims
 x_bound <- 0.125
 y_bound <- 0.35
 
 limx <- c(min(x) - x_bound, max(x) + x_bound)
-limy <- c(min(fx) - y_bound, max(fx) + y_bound)
+limy <- c(min(stationary_fxs) - y_bound, max(stationary_fxs) + y_bound)
+
+dp <- 2
 
 for (model in models) {
-  kernel_name <- str_split(attributes(model[["kernel"]])$class[1], "_")[[1]][3]
+  
+  # Collect info on kernel
+  model_kernel <- model[["kernel"]]
+  kernel_name <- str_split(attributes(model_kernel)$class[1], "_")[[1]][3]
+  dataset_name <- attr(model, "function_name")
+  
+  # Get noise info
+  signal_var <- round(model[["kernel"]]$s2, dp)
+  noise_var <- round(model$nug * signal_var, dp)
+  
+  # Calculate length scale based on model beta
+  beta <- model_kernel$beta
+  length_scale <- round(exp(-beta/2), dp)
+  
+  title_str <- sprintf(
+    " (kernel: %s, dataset: %s, $l$: %s, $\\sigma_f^2$: %s, $\\sigma_n^2$: %s",
+    kernel_name, dataset_name, length_scale, signal_var, noise_var
+  )
+  
+  # Add model parameters if applicable
+  alpha <- model_kernel$alpha
+  if (!is.null(alpha)){
+    title_str <- paste0(title_str, ", $\\alpha$: ", round(alpha, dp))
+  }
+  
+  title_str <- paste0(title_str, ")")
   
   # Extract data-generating function and input data, and produce a line
   model.X_fx_data <- data.frame(
-    X  = model$X,
-    fx = attr(model, "fx")
+    X  = x,
+    fx = attr(model, "function_fx")
   )
   
   model.X_fx <- geom_line(
@@ -142,25 +164,57 @@ for (model in models) {
   # Plot variance in data and in model alongside expected function
   p1 <- model$plot1D() + 
     ggtitle(
-      paste0("Mean and per-sample variances (kernel: ", kernel_name,")"
-    )) + 
+      TeX(
+        paste0(
+          "Mean and per-sample variances", title_str
+        )
+      )
+    ) + 
     model.X_fx # +
     # xlim(limx) + ylim(limy)
   
   # Plot some function drawn from predictive distribution
   p2 <- model$cool1Dplot() + 
     ggtitle(
-      paste0("Draws from function distribution (kernel: ", kernel_name,")"
-    )) + 
+      TeX(
+        paste0(
+          "Draws from function distribution", title_str
+        )
+      )
+    ) + 
     model.X_fx # +
     # xlim(limx) + ylim(limy)
   
   # Plot all graphs
-  print(p1)
-  print(p2)
+  directory <- "img/"
+  metadata_str <- paste0(
+    directory, kernel_name, "_", dataset_name
+  )
+  
+  px_width <- 3207
+  px_height <- 2181
+  dpi <- 300
+  
+  ggsave(
+    filename = paste0(metadata_str, "_mean.png"),
+    plot = p1,
+    width = px_width,
+    height = px_height,
+    units = "px",
+    dpi = dpi
+  )     
+  
+  ggsave(
+    filename = paste0(metadata_str, "_draws.png"),
+    plot = p2,
+    width = px_width,
+    height = px_height,
+    units = "px",
+    dpi = dpi
+  )     
   
   # Print model info
-  print(kernel_name)
+  print(metadata_str)
   print(model$summary())
   print("\n\n")
 }
